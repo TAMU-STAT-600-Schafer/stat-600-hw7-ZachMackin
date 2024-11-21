bb791b020142b21b80f7a70b00ff319f43c9bb6c
Zach <zachmackin@tamu.edu>
2024-11-20 21:03:44 -0600

    modified hyperparams to improve performance

--- Test_letter_NN.R
+++ Test_letter_NN.R
@@ -44,7 +44,16 @@ lines(1:length(out2$error_val), out2$error_val, col = "red")
 
 # Evaluate error on testing data
 test_error = evaluate_error(Xt, Yt, out2$params$W1, out2$params$b1, out2$params$W2, out2$params$b2)
-test_error # 15.71
+test_error # 15.84
 
 # [ToDo] Try changing the parameters above to obtain a better performance,
 # this will likely take several trials
+out3 = NN_train(Xtrain, Ytrain, Xval, Yval, lambda = 0.001,
+                rate = 0.11, mbatch = 50, nEpoch = 150,
+                hidden_p = 100, scale = 0.001, seed = 12345)
+plot(1:length(out3$error), out3$error, ylim = c(0, 70))
+lines(1:length(out3$error_val), out3$error_val, col = "red")
+
+# Evaluate error on testing data
+test_error_3 = evaluate_error(Xt, Yt, out3$params$W1, out3$params$b1, out3$params$W2, out3$params$b2)
+test_error_3 # 14.84
